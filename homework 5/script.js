b6e0422c63d9a3f278105bad965c81890e015344
Danil Titenko <danuxa2009@gmail.com>
2019-12-30 14:59:01 +0200

    HW 5

---
+++ homework 5/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+let action, numberOfOperands, operand, result;
+const arr = [];
+
+
+do {
+  action = prompt("Выбирите действие +, -, /, *");
+} while (action !== "+" && action !== "-" && action !== "*" && action !== "/");
+
+do {
+  numberOfOperands = +prompt("Введите количество операндов");
+} while ((isNaN(numberOfOperands) > 5) || (isNaN(numberOfOperands) < 0));
+
+
+for (let i = 1; i <= numberOfOperands; i++) {
+  do {
+    operand = +prompt('Введите операнд');
+  } while (isNaN(operand && operand !== '' && operand !== ' '));
+  arr.push(operand);
+
+};
+
+
+switch (action) {
+  case '+':
+    result = arr.reduce((summ, start) => summ + start);
+    alert(`Суммирование:${arr.join(' + ')} = ${result}`);
+    break;
+  case '-':
+    result = arr.reduce((sub, start) => sub - start);
+    alert(`Вычитание: ${arr.join(' - ')} = ${result}`);
+    break;
+  case '*':
+    result = arr.reduce((mult, start) => mult * start);
+    alert(`Умножение: ${arr.join(' * ')} = ${result}`);
+    break;
+  case '/':
+    result = arr.reduce((div, start) => div / start);
+    alert(`Деление: ${arr.join(' / ')} = ${result}`);
+    break;
+};
