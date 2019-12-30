ba8415834a5983c5a73243dc57b747e6013d96a9
Danil Titenko <danuxa2009@gmail.com>
2019-12-30 16:31:51 +0200

    пщп

--- homework 5/script.js
+++ homework 5/script.js
@@ -3,40 +3,40 @@
 let action, numberOfOperands, operand, result;
 const arr = [];
 
-
+ывваловпол
 do {
-  action = prompt("Выбирите действие +, -, /, *");
+    action = prompt("Выбирите действие +, -, /, *");
 } while (action !== "+" && action !== "-" && action !== "*" && action !== "/");
 
 do {
-  numberOfOperands = +prompt("Введите количество операндов");
+    numberOfOperands = +prompt("Введите количество операндов");
 } while (isNaN(numberOfOperands) || numberOfOperands > 5 || numberOfOperands < 0);
 
 
 for (let i = 1; i <= numberOfOperands; i++) {
-  do {
-    operand = +prompt('Введите операнд');
-  } while (isNaN(operand && operand !== '' && operand !== ' '));
-  arr.push(operand);
+    do {
+        operand = +prompt('Введите операнд');
+    } while (isNaN(operand && operand !== '' && operand !== ' '));
+    arr.push(operand);
 
 };
 
 
 switch (action) {
-  case '+':
-    result = arr.reduce((summ, start) => summ + start);
-    alert(`Суммирование:${arr.join(' + ')} = ${result}`);
-    break;
-  case '-':
-    result = arr.reduce((sub, start) => sub - start);
-    alert(`Вычитание: ${arr.join(' - ')} = ${result}`);
-    break;
-  case '*':
-    result = arr.reduce((mult, start) => mult * start);
-    alert(`Умножение: ${arr.join(' * ')} = ${result}`);
-    break;
-  case '/':
-    result = arr.reduce((div, start) => div / start);
-    alert(`Деление: ${arr.join(' / ')} = ${result}`);
-    break;
+    case '+':
+        result = arr.reduce((summ, start) => summ + start);
+        alert(`Суммирование:${arr.join(' + ')} = ${result}`);
+        break;
+    case '-':
+        result = arr.reduce((sub, start) => sub - start);
+        alert(`Вычитание: ${arr.join(' - ')} = ${result}`);
+        break;
+    case '*':
+        result = arr.reduce((mult, start) => mult * start);
+        alert(`Умножение: ${arr.join(' * ')} = ${result}`);
+        break;
+    case '/':
+        result = arr.reduce((div, start) => div / start);
+        alert(`Деление: ${arr.join(' / ')} = ${result}`);
+        break;
 };
