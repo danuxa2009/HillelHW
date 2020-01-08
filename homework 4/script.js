2e0ba84e77a58ab971e31a1c1634dae39eab9ed8
Danil Titenko <danuxa2009@gmail.com>
2020-01-08 21:47:34 +0200

    hw 4

--- homework 4/script.js
+++ homework 4/script.js
@@ -1,11 +1,18 @@
 'use strict'
 
-const enteredNumbers = prompt('Введите числа через запятую');
-const arrSplited = enteredNumbers.split(',');
-console.log(arrSplited);
+const enteredNumbers = prompt('Введите числа через запятую')
+    .split(',')
+    .filter(value => !isNaN(value) && value.trim() !== '')
+    .map(Number);
+alert(`Числа в массиве: ${enteredNumbers}`);
 
-const numberArr = arrSplited.filter(number => {
-    return !isNaN(number) && number !== " " && number !== "";
-});
 
-alert(`Числа в массиве ${numberArr.join(',')}`);
+const maxNumber = Math.max(...enteredNumbers);
+alert(`Максимальное число: ${maxNumber}`);
+
+const sumNumbers = enteredNumbers.reduce((acc, num) => acc + num, 0);
+alert(`Сумма всех чисел: ${sumNumbers}`);
+
+
+const evenNumber = enteredNumbers.filter(num => num % 2 === 0);
+alert(`Четные числа: ${evenNumber}`);
