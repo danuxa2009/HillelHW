86b055593cc8d207fe05b8ca10d525c725fa4573
Danil Titenko <danuxa2009@gmail.com>
2020-02-06 23:42:57 +0200

    done

--- homework 14/script.js
+++ homework 14/script.js
@@ -5,27 +5,27 @@ const title = document.querySelector('#title');
 const completedEl = document.querySelector('#completed')
 
 
-fetch('https://jsonplaceholder.typicode.com/todos').then(response => {
-  response.json().then(function (data) {
-    data.map(data => {
-      renderTitle([data.title]),
-        renderCompleted([data.completed])
-      console.log(data)
-    })
+fetch('https://jsonplaceholder.typicode.com/todos')
+  .then(response => {
+    response.json()
+      .then(function (data) {
+        data.forEach(data => {
+          renderTitle(data.title),
+            renderCompleted(data.completed)
+        })
+      });
   });
-});
 
 
 function renderTitle(data) {
-  const titleEl = document.createElement('tr');
+  const titleEl = document.createElement('li');
   titleEl.innerHTML = data;
 
   title.append(titleEl);
 };
 
 function renderCompleted(data) {
-  const completed = document.createElement('tr');
+  const completed = document.createElement('li');
   completed.innerHTML = data;
   completedEl.append(completed)
-
 }
