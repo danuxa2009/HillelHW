c1005d04e8a2d67966b5bade53b3fcb13437b034
Danil Titenko <danuxa2009@gmail.com>
2020-02-17 15:22:40 +0200

    not done

---
+++ homework 17/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const addNewSticker = document.querySelector("#addNewSticker");
+const areaForStickers = document.querySelector("#areaForStickers");
+
+addNewSticker.addEventListener("click", newSticker);
+
+
+function newSticker() {
+  const sticker = document.createElement("div");
+  const elemInput = document.createElement("input");
+
+  const deleteButton = document.createElement('button');
+  deleteButton.classList = 'deleteBtn'
+  deleteButton.innerHTML = 'X';
+  deleteButton.addEventListener('click', onDeleteBtnClick)
+
+  sticker.classList = "newTask";
+  sticker.insertAdjacentElement("afterbegin", elemInput);
+  sticker.insertAdjacentElement('afterbegin', deleteButton)
+  areaForStickers.append(sticker);
+}
+
+
+function onDeleteBtnClick(e) {
+  switch (true) {
+    case e.target.classList.contains('deleteBtn'):
+      deleteSticker(e.target)
+      break;
+  }
+}
+
+function deleteSticker() {
+  const stickerBody = event.target.closest('.newTask')
+  stickerBody.remove();
+}
