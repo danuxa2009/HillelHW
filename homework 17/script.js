165395887931b311dff14bae6f28f12b294be948
Danil Titenko <danuxa2009@gmail.com>
2020-02-21 16:19:24 +0200

    Lesson jquery

--- homework 17/script.js
+++ homework 17/script.js
@@ -7,30 +7,44 @@ addNewSticker.addEventListener("click", newSticker);
 
 
 function newSticker() {
-  const sticker = document.createElement("div");
-  const elemInput = document.createElement("input");
+    const sticker = document.createElement("div");
+    const elemInput = document.createElement("input");
 
-  const deleteButton = document.createElement('button');
-  deleteButton.classList = 'deleteBtn'
-  deleteButton.innerHTML = 'X';
-  deleteButton.addEventListener('click', onDeleteBtnClick)
+    const deleteButton = document.createElement('button');
+    deleteButton.classList = 'deleteBtn'
+    deleteButton.innerHTML = 'X';
+    deleteButton.addEventListener('click', onDeleteBtnClick)
 
-  sticker.classList = "newTask";
-  sticker.insertAdjacentElement("afterbegin", elemInput);
-  sticker.insertAdjacentElement('afterbegin', deleteButton)
-  areaForStickers.append(sticker);
+    sticker.classList = "newTask";
+    sticker.insertAdjacentElement("afterbegin", elemInput);
+    sticker.insertAdjacentElement('afterbegin', deleteButton)
+    areaForStickers.append(sticker);
+    saveItems(elemInput);
+    setId(elemInput);
 }
 
 
 function onDeleteBtnClick(e) {
-  switch (true) {
-    case e.target.classList.contains('deleteBtn'):
-      deleteSticker(e.target)
-      break;
-  }
+    switch (true) {
+        case e.target.classList.contains('deleteBtn'):
+            deleteSticker(e.target)
+            break;
+    }
 }
 
 function deleteSticker() {
-  const stickerBody = event.target.closest('.newTask')
-  stickerBody.remove();
+    const stickerBody = event.target.closest('.newTask')
+    stickerBody.remove();
+}
+
+function setId(elemInput) {
+    const newId = Date.now();
+    elemInput.id = newId;
+
+}
+
+function saveItems(elemInput) {
+    const field = document.getElementById('newId');
+    sessionStorage.setItem('title', field.value);
+
 }
