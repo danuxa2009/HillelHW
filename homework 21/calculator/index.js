c633e7aecfa931be3994fae56003b9fb455e6ebb
Danil Titenko <danuxa2009@gmail.com>
2020-03-01 22:36:49 +0200

    Introduction to Node JS

---
+++ homework 21/calculator/index.js
@@ -0,0 +1,11 @@
+const add = require('./add');
+const div = require('./div');
+const mult = require('./mult');
+const sub = require('./sub');
+
+module.exports = {
+    add: add,
+    div: div,
+    mult: mult,
+    sub: sub
+}
